1cb9ecee6c92229eacbffd9eb8d9e747acae9847
Walisson Silva <walissonsilva10@gmail.com>
2022-11-13 15:47:47 -0300

    feat: create Login screen

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -4,16 +4,20 @@ import LoginImage from "../assets/login.png";
 
 export const Login: React.FC = () => {
   return (
-    <Flex flex={1} justifyContent="center" alignItems="center">
+    <Flex flex={1} justifyContent="center" alignItems="center" p="10">
       <Text
-        fontSize={36}
+        fontSize={30}
         fontWeight={700}
         color="primary.500"
         textAlign="center"
+        lineHeight={34}
       >
-        {`Bem-vindo(a) ao\n Minhas Despesas!`}
+        {`Despesas Pessoais`}
       </Text>
-      <Image source={LoginImage} alt="Login" width={40} height={40} />
+      <Text color="gray.600" fontSize={20} textAlign="center" margin="4">
+        Controle seus gastos mensais e fique mais tranquilo com o seu orçamento.
+      </Text>
+      <Image source={LoginImage} alt="Login" width="48" height="48" />
     </Flex>
   );
 };
