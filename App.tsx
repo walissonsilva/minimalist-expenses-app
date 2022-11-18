7ce9fb1a49981fb227d4279866ea967f045aaeba
Walisson Silva <walissonsilva10@gmail.com>
2022-11-18 18:22:48 -0300

    chore: add react query library

--- App.tsx
+++ App.tsx
@@ -7,11 +7,14 @@ import {
   useFonts,
 } from "@expo-google-fonts/barlow";
 import { NavigationContainer } from "@react-navigation/native";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { NativeBaseProvider, StatusBar } from "native-base";
 import { AuthProvider } from "./src/hooks/useAuth";
 import { FirebaseProvider } from "./src/hooks/useFirebase";
 import { AppRoutes } from "./src/routes/app.routes";
 import { theme } from "./src/styles/theme";
+
+const queryClient = new QueryClient();
 
 export default function App() {
   let [fontsLoaded] = useFonts({
@@ -28,13 +31,15 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={theme}>
-      <AuthProvider>
-        <FirebaseProvider>
-          <NavigationContainer>
-            <AppRoutes />
-          </NavigationContainer>
-        </FirebaseProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <FirebaseProvider>
+            <NavigationContainer>
+              <AppRoutes />
+            </NavigationContainer>
+          </FirebaseProvider>
+        </AuthProvider>
+      </QueryClientProvider>
 
       <StatusBar
         barStyle="light-content"
