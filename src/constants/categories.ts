f9c1e7aceced5693fe54312eeaac36a8cd19b652
Walisson Silva <walissonsilva10@gmail.com>
2022-11-21 00:00:14 -0300

    feat: create categories

---
+++ src/constants/categories.ts
@@ -0,0 +1,22 @@
+import { ICategory } from "../types/ICategory";
+
+export const categories: ICategory[] = [
+  { title: "Alimentação", color: "#ef4444" },
+  { title: "Assinaturas", color: "#f97316" },
+  { title: "Mercado", color: "#84cc16" },
+  { title: "Casa", color: "#06b6d4" },
+  { title: "Compras", color: "#6366f1" },
+  { title: "Cuidados Pessoais", color: "#ec4899" },
+  { title: "Educação", color: "#d946ef" },
+  { title: "Família", color: "#8b5cf6" },
+  { title: "Férias", color: "#10b981" },
+  { title: "Igreja", color: "#0891b2" },
+  { title: "Lazer", color: "#115e59" },
+  { title: "Pets", color: "#67e8f9" },
+  { title: "Pets", color: "#bef264" },
+  { title: "Presentes", color: "#fcd34d" },
+  { title: "Saúde", color: "#fca5a5" },
+  { title: "Trabalho", color: "#374151" },
+  { title: "Transporte", color: "#db2777" },
+  { title: "Viagem", color: "#818cf8" },
+];
