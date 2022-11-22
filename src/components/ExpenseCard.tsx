d0ee1423fde680c27f52a112ea61094c9bbd9429
Walisson Silva <walissonsilva10@gmail.com>
2022-11-21 23:33:22 -0300

    feat: improvements on category exihibition

--- src/components/ExpenseCard.tsx
+++ src/components/ExpenseCard.tsx
@@ -1,6 +1,8 @@
-import { Flex, Text } from "native-base";
+import { Box, Flex, Icon, Text } from "native-base";
+import { categories } from "../constants/categories";
 import { IExpense } from "../types/Expense";
 import { formatDate } from "../utils/date";
+import Feather from "react-native-vector-icons/Feather";
 
 type ExpenseCardProps = Omit<IExpense, "id">;
 
@@ -10,6 +12,18 @@ export const ExpenseCard: React.FC<ExpenseCardProps> = ({
   category,
   date,
 }) => {
+  function getCategoryColor() {
+    const categoryColor = categories.find((c) => c.title === category)?.color;
+
+    return categoryColor ?? "emerald.700";
+  }
+
+  function getCategoryIcon() {
+    const categoryIcon = categories.find((c) => c.title === category)?.icon;
+
+    return categoryIcon ?? "help";
+  }
+
   return (
     <Flex
       paddingX="6"
@@ -29,8 +43,24 @@ export const ExpenseCard: React.FC<ExpenseCardProps> = ({
         </Text>
       </Flex>
 
-      <Flex flexDir="row" justifyContent="space-between" alignItems="center">
-        <Text color="emerald.700">{category}</Text>
+      <Flex
+        flexDir="row"
+        justifyContent="space-between"
+        alignItems="center"
+        mt={1}
+      >
+        <Flex flexDir="row" alignItems="center">
+          <Icon
+            as={Feather}
+            name={getCategoryIcon()}
+            size={3}
+            color={getCategoryColor()}
+            mr={1}
+          />
+          <Text color={getCategoryColor()} fontSize="sm" fontWeight={500}>
+            {category}
+          </Text>
+        </Flex>
         <Text color="gray.400">{formatDate(new Date(date))}</Text>
       </Flex>
     </Flex>
